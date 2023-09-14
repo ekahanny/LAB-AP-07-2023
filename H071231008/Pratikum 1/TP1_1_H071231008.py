3655d8f7cc1b890081d9d308d80637976f312c17
AndiRiswanda <andiriswandalah@gmail.com>
2023-09-14 20:21:59 +0800

    mengirim tugas pekan 1

--- H071231008/Pratikum 1/TP1_1_H071231008.py
+++ H071231008/Pratikum 1/TP1_1_H071231008.py
@@ -2,9 +2,7 @@ x = int(input('Panjang Sisi x: '))
 y = int(input('Panjang Sisi y: '))
 #mencari nilai z
 
-z = x**2 + y**2
-z = z ** (1/2)
-print (z)
+z = (x**2 + y**2)** 0.5
 
 #mencari luas permukaan
 keliling = x + y + z
