171f375fafdb35f076d31bf96fa064a394dd1f0f
AndiRiswanda <andiriswandalah@gmail.com>
2023-10-11 13:27:24 +0800

    Mengirim Tugas Pekan Ke 5

---
+++ H071231008/Pratikum-5/PT5_2_H071231008.py
@@ -0,0 +1,30 @@
+def kataSingkat(kata):
+    try:
+        panjang = len(kata)
+
+        depan = kata[0]
+        if panjang%2 == 0:
+            tengah = (kata[(panjang//2)-1])+ (kata[panjang//2])
+        else:
+            tengah = (kata[panjang//2]) 
+        belakang = kata[panjang-1]
+
+        return (f"{depan}{tengah}{belakang}")
+    
+    except TypeError: return ("Input Invalid")
+
+
+"""
+testcase
+
+"""
+
+print(kataSingkat("AkuSukaCilok"))
+
+print(kataSingkat("James"))
+
+print(kataSingkat("Bakso"))
+
+print(kataSingkat("Presiden"))
+
+
