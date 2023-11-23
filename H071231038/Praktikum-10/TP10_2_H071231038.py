9e9bf3cb34a527cea7d60b6f48280250ee948a8c
IvanBetrandi <ivanbetrandi200603@gmail.com>
2023-11-23 08:50:19 +0800

    Kumpul ulang TP 10 nomor 2

--- H071231038/Praktikum-10/TP10_2_H071231038.py
+++ H071231038/Praktikum-10/TP10_2_H071231038.py
@@ -1,5 +1,13 @@
-class Gun:
+from abc import ABC, abstractmethod
+
+class Gun(ABC):
+    @abstractmethod
+    def action(self):
+        pass
+
+class Pistol(Gun):
     def __init__(self,model,caliber):
+        self.model = model
         self.model = model
         self._caliber = caliber
         self.ammocap = 0
@@ -38,16 +46,27 @@ class Gun:
                 print(self.detail())
             elif act == "g" or act == "G":
                 break
-    
-
-class Pistol(Gun):
-    def __init__(self, model, caliber):
-        super().__init__(model, caliber)
 
 class AssaultRiffle(Gun):
     def __init__(self, model, caliber):
-        super().__init__(model, caliber)
+        self.model = model
+        self.model = model
+        self._caliber = caliber
+        self.ammocap = 0
+        self.ammo = self.ammocap
+        self._firerate = 0
         self.burstmode = False
+
+    def getcaliber(self):
+        return self._caliber
+    
+    def getfirerate(self):
+        return self._firerate
+    def setfirerate(self,firerate):
+        self._firerate += firerate
+
+    def detail(self):
+        return(f"{'-'*20}\nModel: {self.model}\nCaliber: {self.getcaliber()}\nAmmo Capacity: {self.ammocap}\nFire Rate: {self.getfirerate()}\n{'-'*20}")
     
     def action(self):
         while True:
@@ -84,77 +103,7 @@ class AssaultRiffle(Gun):
                     self.burstmode = False
             elif act == "g" or act == "G":
                 break
-    
-class SubMachineGun(Gun):
-    def __init__(self, model, caliber):
-        super().__init__(model, caliber)
-        self.silencer = False
-    
-    def action(self):
-        while True:
-            print("Press A to shoot")
-            print("Press R to reload")
-            print("Press G to unequip")
-            print("Press B to see details")
-            print("Press E to attach silencer")
-            act = input("-> ")
-            if act == "a" or act == "A":
-                if self.ammo <= 0:
-                    print("Ammo: 0")
-                    print("Reload")
-                else:
-                    self.ammo -= 1
-                    print(f"Ammo: {self.ammo}")
-            elif act == "r" or act == "R":
-                self.ammo = self.ammocap
-                print(f"ammo: {self.ammocap}")
-            elif act == "b" or act == "B":
-                print(self.detail())
-            elif act == "E" or act == "e":
-                if not self.silencer:
-                    print("Silencer Attached")
-                    self.silencer = True
-                else:
-                    print("Silencer Detached")
-                    self.silencer = False
-            elif act == "g" or act == "G":
-                break
-
-class SniperRifle(Gun):
-    def __init__(self, model, caliber):
-        super().__init__(model, caliber)
-        self.scope = False
-    
-    def action(self):
-        while True:
-            print("Press A to shoot")
-            print("Press R to reload")
-            print("Press G to unequip")
-            print("Press B to see details")
-            print("Press Q to use scope")
-            act = input("-> ")
-            if act == "a" or act == "A":
-                if self.ammo <= 0:
-                    print("Ammo: 0")
-                    print("Reload")
-                else:
-                    self.ammo -= 1
-                    print(f"Ammo: {self.ammo}")
-            elif act == "r" or act == "R":
-                self.ammo = self.ammocap
-                print(f"ammo: {self.ammocap}")
-            elif act == "b" or act == "B":
-                print(self.detail())
-            elif act == "Q" or act == "q":
-                if not self.scope:
-                    print("Scope ON")
-                    self.scope = True
-                else:
-                    print("Scope OFF")
-                    self.scope = False
-            elif act == "g" or act == "G":
-                break
-    
+            
 
 gun1 = Pistol("Glock 17","9 mm")
 gun1.ammocap = 17
@@ -166,28 +115,16 @@ gun2.ammocap = 30
 gun2.ammo = gun2.ammocap
 gun2._firerate = 600
 
-gun3 = SubMachineGun("MAC-10",".45 ACP")
-gun3.ammocap = 30
-gun3.ammo = gun3.ammocap
-gun3._firerate = 1090
-
-gun4 = SniperRifle("M107A1",".50 BMG")
-gun4.ammocap = 10
-gun4.ammo = gun4.ammocap
-gun4._firerate = 10
-
 while True:
     print("Choose:")
-    print(f"1.{gun1.model}\n2.{gun2.model}\n3.{gun3.model}\n4.{gun4.model}")
-    x = int(input("-> "))
-    if x == 1:
+    print(f"1.{gun1.model}\n2.{gun2.model}\n(Press X to Exit)")
+    x = input("-> ")
+    if x == "1":
         gun1.action()
-    elif x == 2:
+    elif x == "2":
         gun2.action()
-    elif x == 3:
-        gun3.action()
-    elif x == 4:
-        gun4.action()    
-    break
-
+    elif x == "X" or x == "x":
+        break
+    else:
+        print("Invalid input")
         
